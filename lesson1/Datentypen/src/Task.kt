eeb411cb7e3a1ec2691d93b9f6dbe92565f53034
nemig <nicolai.emig@amseegym.de>
2020-09-24 21:03:21 +0200

    Hiding

--- lesson1/Datentypen/src/Task.kt
+++ lesson1/Datentypen/src/Task.kt
@@ -1,21 +1,41 @@
 object Task{
-    fun returnByteDataType() : Byte = TODO()
+    fun returnByteDataType() : Byte {
+        val wert : Byte  = 5
+        return wert
+    }
+    fun returnIntDataType() : Int {
+        val wert  = 5
+        return wert
+    }
+    fun returnShortDataType() : Short {
+        val wert : Short = 5
+        return wert
+    }
+    fun returnLongDataType() : Long {
+        val wert : Long = 5
+        return wert
+    }
+    fun returnBooleanDataType() : Boolean {
+        val wert  = true
+        return wert
+    }
+    fun returnDoubleDataType() : Double {
+        val wert  = 5.5
+        return wert
+    }
+    fun returnFloatDataType() : Float {
+        val wert  = 5f
+        return wert
+    }
+    fun returnCharDataType() : Char {
+        val wert  = '5'
+        return wert
+    }
+    fun returnStringDataType() : String {
+        val wert  = "5"
+        return wert
+    }
 
-    fun returnIntDataType() : Int = TODO()
-
-    fun returnShortDataType() : Short = TODO()
-
-    fun returnLongDataType() : Long = TODO()
-
-    fun returnBooleanDataType() : Boolean = TODO()
-
-    fun returnDoubleDataType() : Double = TODO()
-
-    fun returnFloatDataType() : Float = TODO()
-
-    fun returnCharDataType() : Char = TODO()
-
-    fun returnStringDataType() : String = TODO()
 }
 
 
